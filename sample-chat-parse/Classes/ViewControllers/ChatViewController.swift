554d251bdd03fe9472e72b6e7123acc60bbb77fa
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-11-17 11:11:00 +0200

    Added dateSent value to attachment message send

--- sample-chat-parse/Classes/ViewControllers/ChatViewController.swift
+++ sample-chat-parse/Classes/ViewControllers/ChatViewController.swift
@@ -253,6 +253,7 @@ class ChatViewController: QMChatViewController, QMChatServiceDelegate, UIActionS
             let message = QBChatMessage()
             message.senderID = ServicesManager.instance().currentUser().ID
             message.dialogID = weakSelf?.dialog?.ID
+            message.dateSent = NSDate()
             
             // Sending attachment.
             ServicesManager.instance().chatService.chatAttachmentService.sendMessage(message, toDialog: weakSelf?.dialog, withChatService: ServicesManager.instance().chatService, withAttachedImage: resizedImage, completion: { (error: NSError!) -> Void in
