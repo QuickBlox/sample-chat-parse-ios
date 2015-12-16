f0bce210b9df040fe59abc9385d030c9326b6372
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-12-16 16:08:45 +0200

    Added check for auth to prevent showing progress hud on app start

--- sample-chat-parse/Classes/ViewControllers/DialogsViewController.swift
+++ sample-chat-parse/Classes/ViewControllers/DialogsViewController.swift
@@ -90,7 +90,7 @@ class DialogsViewController: UITableViewController, QMChatServiceDelegate, QMCha
         
         NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationDidBecomeActiveNotification, object: nil, queue: NSOperationQueue.mainQueue()) { (notification: NSNotification) -> Void in
             
-            if !QBChat.instance().isConnected() {
+            if ServicesManager.instance().isAuthorized() && !QBChat.instance().isConnected() {
                 SVProgressHUD.showWithStatus("SA_STR_CONNECTING_TO_CHAT".localized, maskType: SVProgressHUDMaskType.Clear)
             }
         }
